f0cdecc3d2c79a4047055b6fc08e3b3e3807bb77
Tiago Lopes <saxtiago14@gmail.com>
2022-02-27 15:36:19 -0300

    Criando mais de um usuário e perfis de acesso em memória.

---
+++ src/main/java/springSecurity/springSecurity/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springSecurity.springSecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author tiago
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+                .roles("USERS")
+                .and()
+                .withUser("admin")
+                .password("{noop}adm123")
+                .roles("MANAGERS");
+    }
+
+}
