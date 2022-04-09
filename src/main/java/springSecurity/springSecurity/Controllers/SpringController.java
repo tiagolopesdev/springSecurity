78df6a2c488df438c1feeb8a162782dd0388831a
tiagoLopes <saxtiago14@gmail.com>
2022-04-09 17:02:42 -0300

    Requisição para login implementada no controller.

---
+++ src/main/java/springSecurity/springSecurity/Controllers/SpringController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package springSecurity.springSecurity.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+     * @author tiagolopes
+ */
+@Controller
+public class SpringController {
+    
+    @RequestMapping(method = RequestMethod.GET, path = "/login")
+    public String login() {
+        return "entrar";
+    }
+
+}
