b205980f41ffc8f762ef07a34958c4689f96d1e1
tiagoLopes <saxtiago14@gmail.com>
2022-04-24 16:50:45 -0300

    Autorizando URI para role do user e liberando CSS, JS do Spring Security.

--- src/main/java/springSecurity/springSecurity/Config/WebSecurityConfig.java
+++ src/main/java/springSecurity/springSecurity/Config/WebSecurityConfig.java
@@ -4,11 +4,17 @@
  */
 package springSecurity.springSecurity.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import springSecurity.springSecurity.security.UserDetailService;
 
 /**
  *
@@ -18,19 +24,35 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
+    @Autowired
+    private UserDetailService userDetailService;
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/fonts/**").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/newAccount").permitAll()
+                .antMatchers("/").hasRole("USER")
                 .anyRequest().authenticated()
-                .and().formLogin().loginPage("/entrar").permitAll()
-                .and().logout().logoutSuccessUrl("/entrar?logout").permitAll();
+                .and()
+                .formLogin().loginPage("/entrar").permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/entrar?logout").permitAll()
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(3600).rememberMeParameter("remember")
+                .rememberMeCookieName("rememberLogin");
     }
-    
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**",
+                "/css/**", "/fonts/**", "/js/**");
+    }    
+
 }
